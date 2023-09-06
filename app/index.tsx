9531c707cd29ee39945606045acc126479e24c6a
jephthah <jephthah.mbah@outlook.com>
2023-09-06 20:21:40 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Page = () => {
+  return (
+    <View>
+      <Text>This is my first react native app</Text>
+    </View>
+  );
+};
+
+export default Page;
