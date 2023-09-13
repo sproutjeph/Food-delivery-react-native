9001bef586893ce5be7b4e20701a38af3de8e0ff
jephthah <jephthah.mbah@outlook.com>
2023-09-13 22:59:24 +0100

    added filter screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
-import { Stack } from "expo-router";
+import { Stack, useNavigation } from "expo-router";
 import CustomHeader from "../components/CustomHeader";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import Colors from "../constants/Colors";
+import { Touchable } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Ionicons } from "@expo/vector-icons";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
@@ -8,6 +12,7 @@ export const unstable_settings = {
 };
 
 export default function RootLayoutNav() {
+  const navigation = useNavigation();
   return (
     <BottomSheetModalProvider>
       <Stack>
@@ -15,6 +20,30 @@ export default function RootLayoutNav() {
           name="index"
           options={{
             header: () => <CustomHeader />,
+          }}
+        />
+        <Stack.Screen
+          name="(modal)/Filter"
+          options={{
+            presentation: "modal",
+            headerTitle: "Filter",
+            headerShadowVisible: false,
+            headerStyle: {
+              backgroundColor: Colors.lightGrey,
+            },
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.goBack();
+                }}
+              >
+                <Ionicons
+                  name="close-outline"
+                  size={28}
+                  color={Colors.primary}
+                />
+              </TouchableOpacity>
+            ),
           }}
         />
       </Stack>
--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -23,7 +23,7 @@ const SearchBar = () => (
           placeholder="Restaurants, groceries, dishes"
         />
       </View>
-      <Link href={"/"} asChild>
+      <Link href={"/(modal)/Filter"} asChild>
         <TouchableOpacity style={styles.optionButton}>
           <Ionicons name="options-outline" size={20} color={Colors.primary} />
         </TouchableOpacity>
